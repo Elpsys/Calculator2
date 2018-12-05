b8349bbcfff40826065ecb67677f741415362eda
s20171103186 <s20171103186@admindeiMac-34.local>
2018-12-05 10:02:01 +0800

    start
    a new start

--- Calculator2/ViewController.swift
+++ Calculator2/ViewController.swift
@@ -8,8 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    @IBAction func change(_ sender: UIButton) {
+    }
+    @IBAction func percent(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    
+    @IBAction func divide(_ sender: UIButton) {
+    }
+    @IBAction func subtract(_ sender: UIButton) {
+    }
+    @IBAction func add(_ sender: UIButton) {
+    }
+    
+    @IBAction func equel(_ sender: UIButton) {
+    }
+    @IBAction func number1(_ sender: UIButton) {
+    }
+    @IBAction func number2(_ sender: UIButton) {
+    }
+    @IBAction func number3(_ sender: UIButton) {
+    }
+    @IBAction func number4(_ sender: UIButton) {
+    }
+    @IBAction func number5(_ sender: UIButton) {
+    }
+    @IBAction func number6(_ sender: UIButton) {
+    }
+    @IBAction func number7(_ sender: UIButton) {
+    }
+    @IBAction func number8(_ sender: UIButton) {
+    }
+    @IBAction func number9(_ sender: UIButton) {
+    }
+    @IBAction func number0(_ sender: UIButton) {
+    }
+    @IBAction func point(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
