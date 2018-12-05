bade71907de19036dec2fa9d8f3f164f181667b4
李靖韬 <s20171103186@admindeimac-34.local>
2018-12-05 09:04:51 +0800

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by s20171103186 on 2018/12/5.
+//  Copyright © 2018 s20171103186. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
